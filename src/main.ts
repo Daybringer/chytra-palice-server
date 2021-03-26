42822520e60bcac8f3be7fa4d1ba3a5e200107e5
Daybringer <vanata.michal@gmail.com>
2021-03-26 16:29:10 +0100

    added Swagger REST API Documentation starter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger OpenAPI Documentation config
+  const config = new DocumentBuilder()
+    .setTitle('Chytrá palice')
+    .setDescription('API Description ')
+    .setVersion('0.0.1')
+    .addTag('contest')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
